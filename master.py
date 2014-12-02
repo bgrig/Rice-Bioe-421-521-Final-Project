0b2bf52e2c575438ddbc78ad7a45a2d423cf16f3
jacob964 <pi@raspberrypi>
2014-12-02 13:00:01 -0600

    Branch master.py_v1.2

--- master.py
+++ master.py
@@ -8,7 +8,7 @@ import re
 import glob
 
 #Output stdout to log file
-#sys.stdout = open("log.txt", "a")
+sys.stdout = open("log.txt", "a")
 
 #Port Definitions
 port = "/dev/ttyACM0"
@@ -18,11 +18,16 @@ baud = 115200
 zCount = 0
 
 #Regular Expression Compilation
+###Regular expressions to identify coordinates of the form 0+.0+
 xRE = re.compile(" X(-?[0-9]+[.]?[0-9]*)")
 yRE = re.compile(" Y(-?[0-9]+[.]?[0-9]*)")
 zRE = re.compile(" Z(-?[0-9]+[.]?[0-9]*)")  
 
+###Regular expression to parse an M114 command output and obtain the x, y, and z coordinate values
 M114 = re.compile("Count X: ([0-9]+[.]?[0-9]*)Y:([0-9]+[.]?[0-9]*)Z:([0-9]+[.]?[0-9]*)")
+
+### Regular expression to check if the initial section of a Gcode contains a movement 
+### command (G0, G1, or G28)
 Gmove = re.compile("^G[0-1]{1}|28")
 
 #Timeout length
@@ -30,7 +35,7 @@ serTimeout = 1 #timeout for serial communication
 wait = 1 #seconds to wait before releasing hold on rambo communications
 
 # Z home coordinate
-ZHOME = 105
+ZHOME = 105 # mm; constant for the value marlin stores as the z-value after homing
 
 #Initialize Log information
 print("##################")
@@ -44,7 +49,10 @@ rambo = serial.Serial(port, baud, timeout=serTimeout)
 rambo.open()
 
 #Cycle through Marlin initial communications
-timer = 0
+### Marlin outputs certain information to the serial port after initializing a connection
+### This code just clears the output
+timer = 0 #Timer allows for the buffer to catch all communication (pauses briefly to make
+# sure that buffer is clear
 while True:
     response = rambo.read(10000)    
     if response != '':
@@ -55,6 +63,8 @@ while True:
     if timer == 3:
         break
 
+
+# Quick function to check if something is a move command
 def isMove(line):
     result = Gmove.search(line)
 
@@ -63,6 +73,7 @@ def isMove(line):
     else:
         return False
 
+# Function to clear the serial connection buffer between movement commands
 def clearBuffer(ser):
     timer = 0
     while True:
@@ -75,14 +86,21 @@ def clearBuffer(ser):
         else:
             timer += 1
 
+# Function for waiting on z-home command
 def homeWait(zFinal, ser=rambo):
     print("Z coordinate: " + str(zFinal))
     time.sleep(1)
+    
+    # Loop that queries for the current position (M114 command), parses the command
+    # for the z-coordinate, and compares the z-coordinate with the desired destination
+    # coordinate to determine if the movement has finished
     while True:
         currPosition = ''
-
+       
         ser.write("M114\n")
         timer = 0
+        
+        #Loop to get the M114 coordinates
         while True:
             response = ser.read(1000)
             response.strip()
@@ -96,22 +114,27 @@ def homeWait(zFinal, ser=rambo):
                 break
             else:
                 timer += 1
+        
+        #If the buffer value does not exist, then the M114 command has not responded
         if len(currPosition) < 2:
             print("Current Position not found")
             continue
-
+        
+        #Parse M114 command and find position.
         currXYZ = M114.search(currPosition)
         print(currPosition)
 
         zCurr = currXYZ.group(3)
         print("Current Z: " + str(zCurr))
-        
+
+        #Compare destination zValue with current z-position and end wait period if the values are the same        
         if ((float(zCurr) - float(zFinal))**2 < 0.01):
             global zCount
             zCount = ZHOME
             time.sleep(1)
             break
 
+#See comments on waitHome function (just above); Same basic structure, but for G0/1 commands
 def waitOnMove(line, ser=rambo):
     global zCount
     print("waitOnMove Line: " + line.strip())
@@ -170,6 +193,7 @@ def waitOnMove(line, ser=rambo):
             time.sleep(1)
             break
 
+# Function to send gcode Lines over the serial connection.
 def gcodeLine(line, ser=rambo):
     ser.write(line)
     print(line.strip())
@@ -177,25 +201,31 @@ def gcodeLine(line, ser=rambo):
     waitOnMove(line, ser)
     #clearBuffer(ser)
 
+# Uses fbi to change the background to a completely black background
 def initBackground():
     os.system("sudo fbi -T 2 --noverbose ./Background/solid_black.jpg")
     time.sleep(5)
     return
 
+# Uses fbi to display a specified image for a specified number of seconds
 def displayImage(imageName, seconds):
     bashCommand = "sudo fbi -T 2 --noverbose --once -t {0} {1}".format(seconds, imageName)
     os.system(bashCommand)
     time.sleep(seconds+3)
 
+# kills the fbi command to return the system to the original GUI terminal
 def exitFBI():
     os.system("sudo kill $(pgrep fbi)")
 
+# Loads all images from the specified directory
 def loadSlideshow(dir,ext="png"):
     pathname = "./{0}/*.{1}".format(dir, ext)
     images = glob.glob(pathname)
     images.sort()
     return images
 
+# Dummy function to look inside the Creation Workshop output gcode and identify 
+# commands/information prior to actual slicing information
 def parseHeader(f):
     header = []
     for line in f:
@@ -204,6 +234,9 @@ def parseHeader(f):
         header.append(line)
     return header
 
+# Parse function to examine Creation workshop output gcode and output a dictionary
+# of the form key => value; slice# => series of gcode commands for that slice
+# Note: a slice is a 2D cross-section of the final 3D image.
 def parseGcode(f):
     gcodeDict = {}
     slice = -1
@@ -249,7 +282,8 @@ def parseGcode(f):
     
     return gcodeDict
         
-
+# Overview function to parse the header and parse the gcode from a creation workshop file
+# Can be expanded to do other functions (very primitive right now)
 def loadGcode(filename, dir="Gcode"):
     pathname = "./{0}/{1}".format(dir, filename)
     f = open(pathname, "r")
@@ -260,18 +294,24 @@ def loadGcode(filename, dir="Gcode"):
     
     return gcode
 
+# Main code segment
 def main():  
+    # Changes the background of the output screen to black
     initBackground()
 
-
+    # Home the z-axis and prepare for movement
     gcodeLine("G28 Z\n")
     #gcodeLine("G21\n")
     gcodeLine("G91\n")
     gcodeLine("G0 Z-10\n")
 
+    # Load image names from file
     images = loadSlideshow("Testing")
+
+    # Load the gcode data from the creation workshop file
     gcodeDict = loadGcode("0.3-0.5mm_holes_horizontal_short.gcode", ".")
 
+    # Iterate through the slice numbers and execute gcode movements/slideshow image displays with correct timing
     for slice in range(0,len(gcodeDict)):
         gcode = gcodeDict[str(slice)]
         delay = gcode[0]
@@ -284,17 +324,8 @@ def main():
         seconds = int(delay) / 1000
         displayImage(imageName, seconds)
 
-
     #Close rambo serial communications
     rambo.close()
-
-def main2():
-    
-    images = loadSlideshow("Testing")
-    for image in images:
-        initBackground()
-        displayImage(imageName, 5)
-    
 
 main()
 
